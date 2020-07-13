5e59eecdca14ce591960f3e540337d92194f5328
Sergi Orra <sergiorra17@gmail.com>
2020-07-13 19:53:11 +0200

    multi-dimensional slices

---
+++ 004-grouping_data/exercise-7.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// multi-dimensional slices
+	xs1 := []string{"James", "Bond", "Shaken, not stirred"}
+	xs2 := []string{"Miss", "Moneypenny", "Helloooooo, James."}
+	fmt.Println(xs1)
+	fmt.Println(xs2)
+
+	xxs := [][]string{xs1, xs2}
+	fmt.Println(xxs)
+
+	for i, xs := range xxs {
+		fmt.Println("record: ", i)
+		for j, val := range xs {
+			fmt.Printf("\t index position: %v \t value: \t %v \n", j, val)
+		}
+	}
+}
