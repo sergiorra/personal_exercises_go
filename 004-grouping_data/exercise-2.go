fcd7166a261b78543d09e96e50d8950c4ee501ac
Sergi Orra <sergiorra17@gmail.com>
2020-07-13 19:43:50 +0200

    slices

--- 004-grouping_data/exercise-2.go
+++ 004-grouping_data/exercise-2.go
@@ -5,8 +5,8 @@ import (
 )
 
 func main() {
-	// using arrays
-	x := [5]int{42, 43, 44, 45, 46}
+	// using slices
+	x := []int{42, 43, 44, 45, 46, 47, 48, 49, 50, 51}
 	for i, v := range x {
 		fmt.Println(i, v)
 	}
