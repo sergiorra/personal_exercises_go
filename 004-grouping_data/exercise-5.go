24c8a17b9a2ce844234433c0ee71f99f194dc1d7
Sergi Orra <sergiorra17@gmail.com>
2020-07-13 19:48:23 +0200

    deleting from slice using slicing

---
+++ 004-grouping_data/exercise-5.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// deleting from slice using slicing
+	x := []int{42, 43, 44, 45, 46, 47, 48, 49, 50, 51}
+	fmt.Println(x)
+
+	x = append(x[:3], x[6:]...) // [42, 43, 44, 48, 49, 50, 51]
+	fmt.Println(x)
+}
