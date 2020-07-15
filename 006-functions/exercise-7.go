a730b90846660f993c2d2eefed10ad74d5f1d170
Sergi Orra <sergiorra17@gmail.com>
2020-07-15 12:49:23 +0200

    functions as variables

--- 006-functions/exercise-7.go
+++ 006-functions/exercise-7.go
@@ -1,1 +1,29 @@
-package _06_functions
+package main
+
+import (
+	"fmt"
+)
+
+var x int
+var g func()
+
+func main() {
+	// functions as variables
+	f := func() {
+		for i := 0; i < 3; i++ {
+			fmt.Println(i)
+		}
+	}
+
+	f()
+	fmt.Printf("%T\n", f)
+
+	fmt.Println(x)
+	fmt.Printf("%T\n", x)
+
+	g = f
+	g()
+	fmt.Printf("this is g %T\n", g)
+
+	fmt.Println("done")
+}
