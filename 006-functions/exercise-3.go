91a72c372ea53b72751d4e6f896a6bc2810def87
Sergi Orra <sergiorra17@gmail.com>
2020-07-15 12:43:34 +0200

    defer functions

--- 006-functions/exercise-3.go
+++ 006-functions/exercise-3.go
@@ -1,1 +1,18 @@
-package _06_functions
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// defer functions
+	defer foo()
+	fmt.Println("Hello, playground")
+}
+
+func foo() {
+	defer func() {
+		fmt.Println("foo DEFER ran")
+	}()
+	fmt.Println("foo ran")
+}
