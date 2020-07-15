efab5157b41c37c1dd0b6465a267cf26564e04a9
Sergi Orra <sergiorra17@gmail.com>
2020-07-15 12:47:57 +0200

    anonymous function

--- 006-functions/exercise-6.go
+++ 006-functions/exercise-6.go
@@ -1,1 +1,16 @@
-package _06_functions
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// anonymous function
+	func() {
+		for i := 0; i < 100; i++ {
+			fmt.Println(i)
+		}
+	}()
+
+	fmt.Println("done")
+}
