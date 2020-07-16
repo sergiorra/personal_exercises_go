5222e2965ca1b408c738aa4d46a3c8ac0a43d88b
Sergi Orra <sergiorra17@gmail.com>
2020-07-16 11:15:02 +0200

    pointers as reference

--- 007-pointers/exercise-2.go
+++ 007-pointers/exercise-2.go
@@ -1,1 +1,24 @@
-package _07_pointers
+package main
+
+import (
+	"fmt"
+)
+
+type person struct {
+	name string
+}
+
+func main() {
+	// passing pointers as reference
+	p1 := person{
+		name: "James Bond",
+	}
+	fmt.Println(p1)
+	changeMe(&p1)
+	fmt.Println(p1)
+}
+
+func changeMe(p *person) {
+	p.name = "Miss Moneypenny"
+	// (*p).name = "Miss Moneyp"
+}
