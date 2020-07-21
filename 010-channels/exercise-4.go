f2d70ae0a682829e94e0994ac814050f52f4cdfd
Sergi Orra <sergiorra17@gmail.com>
2020-07-21 13:12:35 +0200

    comma ok idiom

--- 010-channels/exercise-4.go
+++ 010-channels/exercise-4.go
@@ -1,1 +1,22 @@
-package _10_channels
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// comma ok idiom
+	c := make(chan int)
+
+	go func() {
+		c <- 42
+	}()
+
+	v, ok := <-c
+	fmt.Println(v, ok)
+
+	close(c)
+
+	v, ok = <-c
+	fmt.Println(v, ok)
+}
