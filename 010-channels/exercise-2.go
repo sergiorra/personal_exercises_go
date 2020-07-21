cdb11ed97c84181a214bc48d76f2c88b967f1ff2
Sergi Orra <sergiorra17@gmail.com>
2020-07-21 13:10:41 +0200

    channels with range loop

--- 010-channels/exercise-2.go
+++ 010-channels/exercise-2.go
@@ -1,1 +1,32 @@
-package _10_channels
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// channels with range loop
+	c := gen()
+	receive(c)
+
+	fmt.Println("about to exit")
+}
+
+func receive(c <-chan int) {
+	for v := range c {
+		fmt.Println(v)
+	}
+}
+
+func gen() <-chan int {
+	c := make(chan int)
+
+	go func() {
+		for i := 0; i < 100; i++ {
+			c <- i
+		}
+		close(c)
+	}()
+
+	return c
+}
