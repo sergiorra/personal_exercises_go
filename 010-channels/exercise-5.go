2ec07851fc3766c615f0ca085d651f5fb985881b
Sergi Orra <sergiorra17@gmail.com>
2020-07-21 13:14:11 +0200

    pulling the numbers off the channel

--- 010-channels/exercise-5.go
+++ 010-channels/exercise-5.go
@@ -1,1 +1,21 @@
-package _10_channels
+package main
+
+import "fmt"
+
+func main() {
+	// pulling the numbers off the channel
+	c := make(chan int)
+
+	go func() {
+		for i := 0; i < 100; i++ {
+			c <- i
+		}
+		close(c)
+	}()
+
+	for v := range c {
+		fmt.Println(v)
+	}
+
+	fmt.Println("about to exit")
+}
