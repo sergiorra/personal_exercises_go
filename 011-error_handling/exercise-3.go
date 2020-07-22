4d1e045315b0093361cb59c533ff856ab96a80c0
Sergi Orra <sergiorra17@gmail.com>
2020-07-22 11:22:51 +0200

    struct that implements error interface

--- 011-error_handling/exercise-3.go
+++ 011-error_handling/exercise-3.go
@@ -1,1 +1,26 @@
-package _11_error_handling
+package main
+
+import (
+	"fmt"
+)
+
+type customErr struct {
+	info string
+}
+
+func (ce customErr) Error() string {
+	return fmt.Sprintf("here is the error: %v", ce.info)
+}
+
+func main() {
+	// struct that implements error interface
+	c1 := customErr{
+		info: "need more coffee",
+	}
+
+	foo(c1)
+}
+
+func foo(e error) {
+	fmt.Println("foo ran -", e, "\n")
+}
