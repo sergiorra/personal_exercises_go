de02abade879e81b5a06bc8726ee789fc819ffe0
Sergi Orra <sergiorra17@gmail.com>
2020-09-28 18:40:27 +0200

    with timeout and cancel function

---
+++ 027-context/exercise-3/go.mod
@@ -0,0 +1,3 @@
+module github.com/sergiorra/personal_exercises_go/027-context/exercise-3
+
+go 1.13
---
+++ 027-context/exercise-3/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+)
+
+func main() {
+	// context with timeout and cancel function
+	ctx := context.Background()
+	ctx, cancelF := context.WithTimeout(ctx, 100*time.Millisecond)
+	defer cancelF()
+	time.Sleep(50 * time.Millisecond)
+	select {
+	case <-ctx.Done():
+		fmt.Println("not finished")
+	default:
+		fmt.Println("finished")
+	}
+}
