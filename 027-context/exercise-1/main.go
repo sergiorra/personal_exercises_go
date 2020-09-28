eb5f2fed7b2ed5b95cf150c25551ed68dfd9ddab
Sergi Orra <sergiorra17@gmail.com>
2020-09-28 18:34:08 +0200

    context with values

---
+++ 027-context/exercise-1/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"context"
+	"fmt"
+)
+
+func main() {
+	// creating a context with values
+	ctx := context.WithValue(context.Background(), "userID", 12345)
+	ctx = context.WithValue(ctx, 1, "admin")
+	if v := ctx.Value("userID"); v != nil {
+		fmt.Println(v)
+	} else {
+		fmt.Println("no value associated with that key")
+	}
+
+	if v := ctx.Value(1); v != nil {
+		fmt.Println(v)
+	} else {
+		fmt.Println("no value associated with that key")
+	}
+
+	if v := ctx.Value(2); v != nil {
+		fmt.Println(v)
+	} else {
+		fmt.Println("no value associated with that key")
+	}
+}
