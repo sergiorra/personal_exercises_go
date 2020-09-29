73d935118a6e6e0a7f9521c2d842a175ac3de853
Sergi Orra <sergiorra17@gmail.com>
2020-09-29 17:06:10 +0200

    error interface

---
+++ 028-errors/exercise-1/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+type errorString string
+
+func (es errorString) Error() string {
+	return fmt.Sprintf("this is an error string with info about who what where when why how - %s", string(es))
+}
+
+func main() {
+	// creating a type that implements the error interface
+	n, err := sum(2, 4)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Println(n)
+}
+
+func sum(i, j int) (int, error) {
+	n := i + j
+	if n != i+j {
+		var sErr errorString = "the numbers didn't add up"
+		return 0, sErr
+	}
+	return n, nil
+}
